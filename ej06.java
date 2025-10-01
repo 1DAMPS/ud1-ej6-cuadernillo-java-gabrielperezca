8aab540176de1c5ac1bb5b18e787d15e0ee88f6c
gabrielperezca <gabriel.perez.carmona.al@iespoligonosur.org>
2025-10-01 14:51:43 +0000

    Subida

---
+++ ej06.java
@@ -0,0 +1,10 @@
+public class ej06{
+    public static void main (String[]args){
+        int cursoEscolar = 2025;
+        int _alumnos = 20;
+        int $clase = 1;
+        System.out.println("Curso escolar: " + cursoEscolar);
+        System.out.println("Nº alumnos: " + _alumnos);
+        System.out.println("Clase: " + $clase);
+    }
+}
