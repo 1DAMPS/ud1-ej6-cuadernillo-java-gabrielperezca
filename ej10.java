fad104a249ed8618d53ece930be8ffb325373ddb
gabrielperezca <gabriel.perez.carmona.al@iespoligonosur.org>
2025-10-07 19:04:24 +0200

    Add files via upload

---
+++ ej10.java
@@ -0,0 +1,9 @@
+public class ej10{
+    public static void main(String[]args){
+        byte centimetros = 120;
+        short paginas = 560;
+
+        System.out.println("byte: " + centimetros);
+        System.out.println("short: " + paginas);
+    }
+}
