ac3cb111e5289b9829f8d2ea21fa229813702ca5
gabrielperezca <gabriel.perez.carmona.al@iespoligonosur.org>
2025-10-07 18:59:52 +0200

    Add files via upload

---
+++ ej08.java
@@ -0,0 +1,13 @@
+public class ej08{
+    public static void main(String[]args){
+        int numero = 1;
+        double pi = 3.141592;
+        char letra = 'C';
+        boolean aprobado = true;
+
+        System.out.println("int: " + numero);
+        System.out.println("double: " + pi);
+        System.out.println("char: " + letra);
+        System.out.println("boolean: " + aprobado);
+    }
+}
